afd3c4e6e54d6acb1541f651bbc50a6a6e5a4663
raphafulldev <raphafulldev@gmail.com>
2023-01-25 21:04:37 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/raphafulldev/course/entities/Category.java
+++ src/main/java/com/raphafulldev/course/entities/Category.java
@@ -1,5 +1,7 @@
 package com.raphafulldev.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,8 +16,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
