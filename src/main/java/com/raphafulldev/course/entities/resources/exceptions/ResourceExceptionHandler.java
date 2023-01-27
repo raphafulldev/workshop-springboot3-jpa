82c2f26246df65a307c894b3c9f833946d77771f
raphafulldev <raphafulldev@gmail.com>
2023-01-27 15:41:23 -0300

    Exception handling - delete

--- src/main/java/com/raphafulldev/course/entities/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/raphafulldev/course/entities/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.raphafulldev.course.entities.resources.exceptions;
 
+import com.raphafulldev.course.services.exceptions.DatabaseException;
 import com.raphafulldev.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -13,10 +14,19 @@ import java.time.Instant;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
--- src/main/java/com/raphafulldev/course/services/UserService.java
+++ src/main/java/com/raphafulldev/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.raphafulldev.course.services;
 
 import com.raphafulldev.course.entities.User;
 import com.raphafulldev.course.repositories.UserRepository;
+import com.raphafulldev.course.services.exceptions.DatabaseException;
 import com.raphafulldev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,24 +18,31 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
 
-    public User findById (Long id){
+    public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
-    public User insert (User obj){
+    public User insert(User obj) {
         return repository.save(obj);
     }
 
-    public void delete (Long id){
-        repository.deleteById(id);
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(e.getMessage());
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
-    public User update (Long id, User obj){
+    public User update(Long id, User obj) {
         User entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
