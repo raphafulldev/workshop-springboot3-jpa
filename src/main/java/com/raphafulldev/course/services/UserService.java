be06a6d7b037d7ba1826e5a9872a5d9092952ac9
raphafulldev <raphafulldev@gmail.com>
2023-01-27 15:29:07 -0300

    Exception handling - findById

--- src/main/java/com/raphafulldev/course/services/UserService.java
+++ src/main/java/com/raphafulldev/course/services/UserService.java
@@ -2,10 +2,12 @@ package com.raphafulldev.course.services;
 
 import com.raphafulldev.course.entities.User;
 import com.raphafulldev.course.repositories.UserRepository;
+import com.raphafulldev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,7 +20,8 @@ public class UserService {
     }
 
     public User findById (Long id){
-        return repository.findById(id).get();
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert (User obj){
---
+++ src/main/java/com/raphafulldev/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.raphafulldev.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException (Object id){
+        super("Resource not found. id " + id);
+    }
+}
